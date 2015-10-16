b47df68e36af2cb057a60f13998e053e683fccb6
Mitchell Bank <mitchbank26@gmail.com>
2015-10-16 16:05:30 -0400

    Loading jquery and still trying to get the DOM respond to our extension

--- twinty.js
+++ twinty.js
@@ -9,19 +9,25 @@
 // #  - on off/on
 
 // insert AJAX here
+document.addEventListener('DOMContentLoaded', function () {
+  alert('yup');
+});
 
-$.ajax('localhost:3000',
-	{
-		method: 'get',
-		datatype: 'json',
-	}).done(function(data) {
-		$('.tweet').each(function(index, value){
-			tweet_id = $(value).attr('data-tweet-id')
 
-			if !$.inArray(tweet_id, data) {
-				$(value).hide();
-			}
-		})
-	})
+// $(document).ready(function(){
+// $.ajax('localhost:3000/home.json',
+// 	{
+// 		method: 'get',
+// 		datatype: 'json',
+// 	}).done(function(data) {
+// 		console.log(data);
+// 		$('.tweet').each(function(index, value){
+// 			tweet_id = $(value).attr('data-tweet-id')
 
-document.onready
+// 			if !$.inArray(tweets_id_array, data) {
+// 				$(value).hide();
+// 			}
+// 		})
+// 	})
+
+// })
